7fe00f055795442d2c1373ee0e5eace1e753016d
Dickon Reed <dickon.reed@r3.com>
2023-06-07 16:46:44 +0100

    CORE-14335: move SigningKeyInfo to libs/crypto/crypto-core so that it can be used in CryptoService

--- components/crypto/crypto-softhsm-impl/src/test/kotlin/net/corda/crypto/softhsm/impl/infra/TestSigningRepository.kt
+++ components/crypto/crypto-softhsm-impl/src/test/kotlin/net/corda/crypto/softhsm/impl/infra/TestSigningRepository.kt
@@ -1,7 +1,7 @@
 package net.corda.crypto.softhsm.impl.infra
 
 import net.corda.crypto.core.ShortHash
-import net.corda.crypto.persistence.SigningKeyInfo
+import net.corda.crypto.core.SigningKeyInfo
 import net.corda.crypto.persistence.SigningKeyOrderBy
 import net.corda.crypto.persistence.SigningWrappedKeySaveContext
 import net.corda.crypto.softhsm.SigningRepository
