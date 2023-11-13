1969f072f938ff4d007c4b8828d478e514b5d890
James Higgs <james.higgs@r3.com>
2023-11-13 17:08:47 +0000

    PROTOTYPE: Handle send and send and receive outputs

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/sessions/SendAndReceiveRequestHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/sessions/SendAndReceiveRequestHandler.kt
@@ -4,23 +4,25 @@ import net.corda.data.flow.state.waiting.SessionData
 import net.corda.data.flow.state.waiting.WaitingFor
 import net.corda.flow.fiber.FlowIORequest
 import net.corda.flow.pipeline.events.FlowEventContext
-import net.corda.flow.pipeline.exceptions.FlowPlatformException
 import net.corda.flow.pipeline.handlers.requests.FlowRequestHandler
 import net.corda.flow.pipeline.handlers.requests.sessions.service.GenerateSessionService
-import net.corda.flow.pipeline.handlers.waiting.sessions.PROTOCOL_MISMATCH_HINT
-import net.corda.flow.pipeline.sessions.FlowSessionManager
-import net.corda.flow.pipeline.sessions.FlowSessionStateException
+import net.corda.flow.session.SessionManager
+import net.corda.flow.session.SessionManagerFactory
+import net.corda.libs.configuration.helper.getConfig
+import net.corda.schema.configuration.ConfigKeys
+import net.corda.virtualnode.HoldingIdentity
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
-import java.time.Instant
 
 @Component(service = [FlowRequestHandler::class])
 class SendAndReceiveRequestHandler @Activate constructor(
-    @Reference(service = FlowSessionManager::class)
-    private val flowSessionManager: FlowSessionManager,
+//    @Reference(service = FlowSessionManager::class)
+//    private val flowSessionManager: FlowSessionManager,
     @Reference(service = GenerateSessionService::class)
     private val generateSessionService: GenerateSessionService,
+    @Reference(service = SessionManagerFactory::class)
+    private val sessionManagerFactory: SessionManagerFactory
 ) : FlowRequestHandler<FlowIORequest.SendAndReceive> {
 
     override val type = FlowIORequest.SendAndReceive::class.java
@@ -33,16 +35,37 @@ class SendAndReceiveRequestHandler @Activate constructor(
         context: FlowEventContext<Any>,
         request: FlowIORequest.SendAndReceive
     ): FlowEventContext<Any> {
-        val checkpoint = context.checkpoint
-
-        //generate init messages for sessions which do not exist yet
-        generateSessionService.generateSessions(context, request.sessionToInfo.keys)
-
-        try {
-            checkpoint.putSessionStates(flowSessionManager.sendDataMessages(checkpoint, request.sessionToInfo, Instant.now()))
-        } catch (e: FlowSessionStateException) {
-            throw FlowPlatformException("Failed to send/receive: ${e.message}. $PROTOCOL_MISMATCH_HINT", e)
+        val sessionManager = sessionManagerFactory.create(
+            context.configs.getConfig(ConfigKeys.STATE_MANAGER_CONFIG),
+            context.configs.getConfig(ConfigKeys.MESSAGING_CONFIG)
+        )
+        val sessionProperties = generateSessionService.createSessionProperties(context)
+        val configs = request.sessionToInfo.keys.associate {
+            it.sessionId to SessionManager.SessionConfig(
+                cpiId = context.checkpoint.flowStartContext.cpiId,
+                party = context.checkpoint.holdingIdentity,
+                counterparty = HoldingIdentity(it.counterparty, context.checkpoint.holdingIdentity.groupId),
+                requireClose = it.requireClose,
+                contextSessionProperties = sessionProperties.avro
+            )
         }
+        configs.forEach {
+            sessionManager.createSession(it.key, it.value)
+        }
+        request.sessionToInfo.forEach {
+            sessionManager.sendMessage(it.key.sessionId, it.value)
+        }
+        sessionManager.close()
+//        val checkpoint = context.checkpoint
+//
+//        //generate init messages for sessions which do not exist yet
+//        generateSessionService.generateSessions(context, request.sessionToInfo.keys)
+//
+//        try {
+//            checkpoint.putSessionStates(flowSessionManager.sendDataMessages(checkpoint, request.sessionToInfo, Instant.now()))
+//        } catch (e: FlowSessionStateException) {
+//            throw FlowPlatformException("Failed to send/receive: ${e.message}. $PROTOCOL_MISMATCH_HINT", e)
+//        }
 
         return context
     }
--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/sessions/SendRequestHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/sessions/SendRequestHandler.kt
@@ -3,23 +3,25 @@ package net.corda.flow.pipeline.handlers.requests.sessions
 import net.corda.data.flow.state.waiting.WaitingFor
 import net.corda.flow.fiber.FlowIORequest
 import net.corda.flow.pipeline.events.FlowEventContext
-import net.corda.flow.pipeline.exceptions.FlowPlatformException
 import net.corda.flow.pipeline.handlers.requests.FlowRequestHandler
 import net.corda.flow.pipeline.handlers.requests.sessions.service.GenerateSessionService
-import net.corda.flow.pipeline.handlers.waiting.sessions.PROTOCOL_MISMATCH_HINT
-import net.corda.flow.pipeline.sessions.FlowSessionManager
-import net.corda.flow.pipeline.sessions.FlowSessionStateException
+import net.corda.flow.session.SessionManager
+import net.corda.flow.session.SessionManagerFactory
+import net.corda.libs.configuration.helper.getConfig
+import net.corda.schema.configuration.ConfigKeys
+import net.corda.virtualnode.HoldingIdentity
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
-import java.time.Instant
 
 @Component(service = [FlowRequestHandler::class])
 class SendRequestHandler @Activate constructor(
-    @Reference(service = FlowSessionManager::class)
-    private val flowSessionManager: FlowSessionManager,
+//    @Reference(service = FlowSessionManager::class)
+//    private val flowSessionManager: FlowSessionManager,
     @Reference(service = GenerateSessionService::class)
     private val generateSessionService: GenerateSessionService,
+    @Reference(service = SessionManagerFactory::class)
+    private val sessionManagerFactory: SessionManagerFactory
 ) : FlowRequestHandler<FlowIORequest.Send> {
 
     override val type = FlowIORequest.Send::class.java
@@ -29,15 +31,36 @@ class SendRequestHandler @Activate constructor(
     }
 
     override fun postProcess(context: FlowEventContext<Any>, request: FlowIORequest.Send): FlowEventContext<Any> {
-        val checkpoint = context.checkpoint
-        try {
-            //generate session states for sessions which do not exist yet
-            generateSessionService.generateSessions(context, request.sessionPayloads.keys)
-            checkpoint.putSessionStates(flowSessionManager.sendDataMessages(checkpoint, request.sessionPayloads, Instant.now()))
-        } catch (e: FlowSessionStateException) {
-            throw FlowPlatformException("Failed to send: ${e.message}. $PROTOCOL_MISMATCH_HINT", e)
+        val sessionManager = sessionManagerFactory.create(
+            context.configs.getConfig(ConfigKeys.STATE_MANAGER_CONFIG),
+            context.configs.getConfig(ConfigKeys.MESSAGING_CONFIG)
+        )
+        val sessionProperties = generateSessionService.createSessionProperties(context)
+        val configs = request.sessionPayloads.keys.associate {
+            it.sessionId to SessionManager.SessionConfig(
+                cpiId = context.checkpoint.flowStartContext.cpiId,
+                party = context.checkpoint.holdingIdentity,
+                counterparty = HoldingIdentity(it.counterparty, context.checkpoint.holdingIdentity.groupId),
+                requireClose = it.requireClose,
+                contextSessionProperties = sessionProperties.avro
+            )
         }
-
+        configs.forEach {
+            sessionManager.createSession(it.key, it.value)
+        }
+        request.sessionPayloads.forEach {
+            sessionManager.sendMessage(it.key.sessionId, it.value)
+        }
+        sessionManager.close()
+//        val checkpoint = context.checkpoint
+//        try {
+//            generate session states for sessions which do not exist yet
+//            generateSessionService.generateSessions(context, request.sessionPayloads.keys)
+//            checkpoint.putSessionStates(flowSessionManager.sendDataMessages(checkpoint, request.sessionPayloads, Instant.now()))
+//        } catch (e: FlowSessionStateException) {
+//            throw FlowPlatformException("Failed to send: ${e.message}. $PROTOCOL_MISMATCH_HINT", e)
+//        }
+//
         return context
     }
 }
