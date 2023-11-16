6c4d35ef720db9feefff945dcadbab3d74bc79f2
James Higgs <james.higgs@r3.com>
2023-11-16 10:38:42 +0000

    PROTOTYPE: Implement session close on subflow exit

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/SubFlowFinishedRequestHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/SubFlowFinishedRequestHandler.kt
@@ -7,52 +7,72 @@ import net.corda.flow.fiber.FlowIORequest
 import net.corda.flow.pipeline.events.FlowEventContext
 import net.corda.flow.pipeline.exceptions.FlowFatalException
 import net.corda.flow.pipeline.handlers.requests.sessions.service.CloseSessionService
+import net.corda.flow.session.SessionManagerFactory
+import net.corda.libs.configuration.helper.getConfig
+import net.corda.schema.configuration.ConfigKeys
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 
 @Component(service = [FlowRequestHandler::class])
 class SubFlowFinishedRequestHandler @Activate constructor(
-    @Reference(service = CloseSessionService::class)
-    private val closeSessionService: CloseSessionService
+//    @Reference(service = CloseSessionService::class)
+//    private val closeSessionService: CloseSessionService,
+    @Reference(service = SessionManagerFactory::class)
+    private val sessionManagerFactory: SessionManagerFactory
 ) : FlowRequestHandler<FlowIORequest.SubFlowFinished> {
 
     override val type = FlowIORequest.SubFlowFinished::class.java
 
-    private fun getSessionsToClose(request: FlowIORequest.SubFlowFinished): List<String> {
-        return request.sessionIds.toMutableList()
-    }
+//    private fun getSessionsToClose(request: FlowIORequest.SubFlowFinished): List<String> {
+//        return request.sessionIds.toMutableList()
+//    }
 
     override fun getUpdatedWaitingFor(
         context: FlowEventContext<Any>,
         request: FlowIORequest.SubFlowFinished
     ): WaitingFor {
-        val sessionsToClose = try {
-            closeSessionService.getSessionsToCloseForWaitingFor(context.checkpoint, getSessionsToClose(request))
-        } catch (e: Exception) {
-            val msg = e.message ?: "An error occurred in the platform - A session in ${request.sessionIds} was missing from the checkpoint"
-            throw FlowFatalException(msg, e)
-        }
-
-        return if (sessionsToClose.isEmpty()) {
-            WaitingFor(net.corda.data.flow.state.waiting.Wakeup())
-        } else {
-            WaitingFor(SessionConfirmation(sessionsToClose, SessionConfirmationType.CLOSE))
-        }
+        return WaitingFor(net.corda.data.flow.state.waiting.Wakeup())
+//        val sessionsToClose = try {
+//            closeSessionService.getSessionsToCloseForWaitingFor(context.checkpoint, getSessionsToClose(request))
+//        } catch (e: Exception) {
+//            val msg = e.message ?: "An error occurred in the platform - A session in ${request.sessionIds} was missing from the checkpoint"
+//            throw FlowFatalException(msg, e)
+//        }
+//
+//        return if (sessionsToClose.isEmpty()) {
+//            WaitingFor(net.corda.data.flow.state.waiting.Wakeup())
+//        } else {
+//            WaitingFor(SessionConfirmation(sessionsToClose, SessionConfirmationType.CLOSE))
+//        }
     }
 
     override fun postProcess(
         context: FlowEventContext<Any>,
         request: FlowIORequest.SubFlowFinished
     ): FlowEventContext<Any> {
-        val checkpoint = context.checkpoint
-        try {
-            closeSessionService.handleCloseForSessions(checkpoint, getSessionsToClose(request))
-        } catch (e: Exception) {
-            // TODO CORE-4850 Wakeup with error when session does not exist
-            val msg = e.message ?: "An error occurred in the platform - A session in ${request.sessionIds} was missing from the checkpoint"
-            throw FlowFatalException(msg, e)
+        val sessionManager = sessionManagerFactory.create(
+            context.configs.getConfig(ConfigKeys.STATE_MANAGER_CONFIG),
+            context.configs.getConfig(ConfigKeys.MESSAGING_CONFIG)
+        )
+        val (initiatedSessions, initiatingSessions) = request.sessionIds.partition {
+            it.endsWith("-INITIATED")
         }
+        initiatedSessions.forEach {
+            sessionManager.deleteSession(it)
+        }
+        initiatingSessions.forEach {
+            sessionManager.deleteSession(it)
+        }
+        sessionManager.close()
+//        val checkpoint = context.checkpoint
+//        try {
+//            closeSessionService.handleCloseForSessions(checkpoint, getSessionsToClose(request))
+//        } catch (e: Exception) {
+//            // TODO CORE-4850 Wakeup with error when session does not exist
+//            val msg = e.message ?: "An error occurred in the platform - A session in ${request.sessionIds} was missing from the checkpoint"
+//            throw FlowFatalException(msg, e)
+//        }
         return context
     }
 }
