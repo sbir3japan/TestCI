94c2585f148b93b9fe70f56e1ccf6021601a543f
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-11-23 17:05:06 +0000

    CORE-15813: Simplify VirtualNodeInfoMap
    While debugging CORE-15813, I noticed this class has a few issues.
    
    Top issue is that it holds two ConcurrentHashMap fields and attempts
    to validate they are in sync but it never does any locking in its own
    methods that modify these fields. Switching threads mid way through
    the methods of this class could present a view of the two maps holding
    different content. That would break the rules this class attempts to
    enforce. To solve this I've made the methods Synchronized.
    
    The getAllAsCordaObjects method built it's return value in two
    steps. It built an in memory map and immediatly threw it away. I've
    updated this method to build the final map from the start.
    
    The put and remove methods had been broken down into methods with a
    focus on which field they were handling. These methods are simple
    enough to do the work in one and the resulting code makes it clear
    they are updating both fields.
    
    Extract validation logic into its own method so it is distinct from
    modifications to the fields.
    
    Remove a validation check that is impossible to hit now we have
    synchronized our own methods.

--- components/virtual-node/virtual-node-info-read-service/src/main/kotlin/net/corda/virtualnode/read/impl/VirtualNodeInfoMap.kt
+++ components/virtual-node/virtual-node-info-read-service/src/main/kotlin/net/corda/virtualnode/read/impl/VirtualNodeInfoMap.kt
@@ -14,72 +14,68 @@ import java.util.concurrent.ConcurrentHashMap
  * This class is nothing more than two maps with different keys.
  */
 internal class VirtualNodeInfoMap {
-    private val virtualNodeInfoByHoldingIdentity =  ConcurrentHashMap<HoldingIdentity, VirtualNodeInfo>()
+    private val virtualNodeInfoByHoldingIdentity = ConcurrentHashMap<HoldingIdentity, VirtualNodeInfo>()
     private val virtualNodeInfoById = ConcurrentHashMap<ShortHash, VirtualNodeInfo>()
 
     /** Class to be used as a key for putting items. */
     data class Key(val holdingIdentity: HoldingIdentity, val holdingIdShortHash: ShortHash)
 
     /** Get everything as Corda objects NOT Avro objects */
+    @Synchronized
     fun getAllAsCordaObjects(): Map<net.corda.virtualnode.HoldingIdentity, net.corda.virtualnode.VirtualNodeInfo> =
-        virtualNodeInfoByHoldingIdentity.mapKeys { it.key.toCorda() }.mapValues { it.value.toCorda() }
+        virtualNodeInfoByHoldingIdentity.asIterable().associate { (key, value) -> key.toCorda() to value.toCorda() }
 
     /** Put (store/merge) the incoming map.  May throw [IllegalArgumentException] */
+    @Synchronized
     fun putAll(incoming: Map<Key, VirtualNodeInfo>) = incoming.forEach { (key, value) -> put(key, value) }
 
-    /** Put [VirtualNodeInfo] into internal maps. May throw [IllegalArgumentException] */
-    private fun putValue(key: Key, value: VirtualNodeInfo) {
+    /** Putting a null value removes the [VirtualNodeInfo] from the maps. May throw [IllegalArgumentException] */
+    @Synchronized
+    fun put(key: Key, value: VirtualNodeInfo?) {
+        if (value == null) {
+            remove(key)
+        } else {
+            validatePut(key, value)
+            virtualNodeInfoById[key.holdingIdShortHash] = value
+            virtualNodeInfoByHoldingIdentity[key.holdingIdentity] = value
+        }
+    }
+
+    private fun validatePut(key: Key, value: VirtualNodeInfo) {
         // The following are checks that "should never occur in production", i.e.
         // that the holding identity 'key' matches the holding identity in the 'value'.
         // Whoever posts (HoldingIdentity, VirtualNodeInfo) on to Kakfa, should have used:
         // (virtualNodeInfo.holdingIdentity, virtualNodeInfo).
-        if (key.holdingIdentity != value.holdingIdentity) {
-            throw IllegalArgumentException("Trying to add a VirtualNodeInfo with a mismatched HoldingIdentity: ($key , $value)")
+        require(key.holdingIdentity == value.holdingIdentity) {
+            "Trying to add a VirtualNodeInfo with a mismatched HoldingIdentity: ($key , $value)"
         }
-        if (virtualNodeInfoById.containsKey(key.holdingIdShortHash)
-            && virtualNodeInfoById[key.holdingIdShortHash]?.holdingIdentity != value.holdingIdentity) {
-            throw IllegalArgumentException("Cannot put different VirtualNodeInfo for same short hash value: " +
-                    "(${key.holdingIdShortHash}, $key , $value)")
-        }
-        virtualNodeInfoById[key.holdingIdShortHash] = value
-        virtualNodeInfoByHoldingIdentity[key.holdingIdentity] = value
-    }
-
-    /** Putting a null value removes the [VirtualNodeInfo] from the maps. May throw [IllegalArgumentException] */
-    fun put(key: Key, value: VirtualNodeInfo?) {
-        if (value != null) {
-            putValue(key, value)
-        } else {
-            remove(key)
+        val existingHoldingIdentity = virtualNodeInfoById[key.holdingIdShortHash]?.holdingIdentity
+        require(existingHoldingIdentity == null || existingHoldingIdentity == value.holdingIdentity) {
+            "Cannot put different VirtualNodeInfo for same short hash value: (${key.holdingIdShortHash}, $key , $value)"
         }
     }
 
     /** Get list of [VirtualNodeInfo] for all virtual nodes. Returns an empty list if no virtual nodes are onboarded. */
+    @Synchronized
     fun getAll(): List<VirtualNodeInfo> = virtualNodeInfoById.values.toList()
 
     /** Get a [VirtualNodeInfo] by [HoldingIdentity], `null` if not found. */
+    @Synchronized
     fun get(holdingIdentity: HoldingIdentity): VirtualNodeInfo? = virtualNodeInfoByHoldingIdentity[holdingIdentity]
 
     /** Get a [VirtualNodeInfo] by short hash ([net.corda.virtualnode.HoldingIdentity.shortHash]), `null` if not found */
+    @Synchronized
     fun getById(holdingIdShortHash: ShortHash): VirtualNodeInfo? = virtualNodeInfoById[holdingIdShortHash]
 
-    /** Remove the [VirtualNodeInfo] for a given key, specifically from the 'by id' map, in this method. */
-    private fun removeById(key: Key) {
-        // yes, this is xor
-        if (virtualNodeInfoById.containsKey(key.holdingIdShortHash) xor virtualNodeInfoByHoldingIdentity.containsKey(key.holdingIdentity)) {
-            throw IllegalArgumentException("Keys should be present or missing in both maps - found key in only one map")
-        }
-
-        virtualNodeInfoById.remove(key.holdingIdShortHash)
-    }
-
     /** Remove the [VirtualNodeInfo] from this collection and return it. */
+    @Synchronized
     fun remove(key: Key): VirtualNodeInfo? {
-        removeById(key)
+        virtualNodeInfoById.remove(key.holdingIdShortHash)
         return virtualNodeInfoByHoldingIdentity.remove(key.holdingIdentity)
     }
 
     /** Clear all the content */
+    @Synchronized
     fun clear() {
         virtualNodeInfoById.clear()
         virtualNodeInfoByHoldingIdentity.clear()
