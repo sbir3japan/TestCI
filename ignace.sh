867e16f8d5b30f0a87e874a61ad482988d64e557
Ignace Loomans <ignace.loomans@gmail.com>
2024-01-04 12:15:14 +0000

    CORE-18575: new ignace.sh script

--- ignace.sh
+++ ignace.sh
@@ -72,16 +72,16 @@ X500="CN=Eve, OU=Application, O=NordVPN, L=Athens, C=GR"
 HOLDING_ID_4=$(curl --insecure -u admin:admin -s -d '{ "request": { "cpiFileChecksum": "'"$CPI_HASH"'", "x500Name": "'"$X500"'"  } }' https://localhost:8888/api/v1/virtualnode | jq -r '.requestId')
 echo "Holding ID 4 = "$HOLDING_ID_4
 
-sleep 1
-curl --insecure -u admin:admin -X PUT -F alias="digicert-ca" -F certificate=@digicert-ca.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
-sleep 1
-keytool -exportcert -rfc -alias "signing key 1" -keystore signingkeys.pfx -storepass "DemoPassword123" -file signingkey1.pem
-sleep 1
-curl --insecure -u admin:admin -X PUT -F alias="signingkey1-2022" -F certificate=@signingkey1.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
-sleep 1
-curl --insecure -u admin:admin -X PUT -F alias="gradle-plugin-default-key" -F certificate=@gradle-plugin-default-key.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
-
-
+#sleep 1
+#curl --insecure -u admin:admin -X PUT -F alias="digicert-ca" -F certificate=@digicert-ca.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
+#sleep 1
+#keytool -exportcert -rfc -alias "signing key 1" -keystore signingkeys.pfx -storepass "DemoPassword123" -file signingkey1.pem
+#sleep 1
+#curl --insecure -u admin:admin -X PUT -F alias="signingkey1-2022" -F certificate=@signingkey1.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
+#sleep 1
+#curl --insecure -u admin:admin -X PUT -F alias="gradle-plugin-default-key" -F certificate=@gradle-plugin-default-key.pem https://localhost:8888/api/v1/certificates/cluster/code-signer
+#
+#
 
 ######################################
 ######################################
@@ -110,6 +110,7 @@ echo "RequestID = "$REQUEST_ID
 sleep 25
 
 curl --insecure -u admin:admin -s https://localhost:8888/api/v1/cpi/status/$REQUEST_ID
+sleep 10
 read STATUS CPI_HASH < <(echo $(curl --insecure -u admin:admin -s https://localhost:8888/api/v1/cpi/status/$REQUEST_ID | jq -r '.status, .cpiFileChecksum'))
 
 printf "\nRequest id = $REQUEST_ID   CPI hash = $CPI_HASH   Status = $STATUS\n\n"
