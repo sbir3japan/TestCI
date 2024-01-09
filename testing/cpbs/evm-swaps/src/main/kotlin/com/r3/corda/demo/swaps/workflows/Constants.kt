2e34c739ae33f7f2338bf9bc39904b6779a9fe29
Ignace Loomans <ignace.loomans@gmail.com>
2024-01-09 17:25:22 +0000

    CORE-18575: fix

--- testing/cpbs/evm-swaps/src/main/kotlin/com/r3/corda/demo/swaps/workflows/Constants.kt
+++ testing/cpbs/evm-swaps/src/main/kotlin/com/r3/corda/demo/swaps/workflows/Constants.kt
@@ -2,6 +2,6 @@ package com.r3.corda.demo.swaps.workflows
 
 class Constants {
     companion object {
-        val RPC_URL = System.getenv("DEMO_RPC_URL") ?: "http://host.docker.internal:8545"
+        val RPC_URL =  "http://host.docker.internal:8545"
     }
 }
