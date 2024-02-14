3c2394393984857f4879f9990bca5c97b3ceca18
Yash Nabar <yash.nabar@r3.com>
2024-02-14 09:20:53 +0000

    Fix persist

--- components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
+++ components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
@@ -7,6 +7,7 @@ import net.corda.membership.datamodel.RegistrationRequestEntity
 import net.corda.membership.impl.persistence.service.handler.RegistrationStatusHelper.toStatus
 import net.corda.membership.lib.registration.RegistrationStatusExt.canMoveToStatus
 import net.corda.orm.utils.use
+import net.corda.virtualnode.HoldingIdentity
 import net.corda.virtualnode.toCorda
 import javax.persistence.LockModeType
 
@@ -17,7 +18,7 @@ internal class PersistRegistrationRequestHandler(
     override fun invoke(context: MembershipRequestContext, request: PersistRegistrationRequest) {
         val registrationId = request.registrationRequest.registrationId
         logger.info("Persisting registration request with ID [$registrationId] to status ${request.status}.")
-        transaction(context.holdingIdentity.toCorda().shortHash) { em ->
+        val previousEntity = transaction(context.holdingIdentity.toCorda().shortHash) { em ->
             val currentRegistrationRequest = em.find(
                 RegistrationRequestEntity::class.java,
                 registrationId,
@@ -28,9 +29,7 @@ internal class PersistRegistrationRequestHandler(
                     logger.info(
                         "Registration request [$registrationId] with status: ${currentRegistrationRequest.status}" + " is already persisted. Persistence request was discarded."
                     )
-                    return@transaction
-                }
-                if (!it.canMoveToStatus(request.status)) {
+                } else if (!it.canMoveToStatus(request.status)) {
                     logger.info(
                         "Registration request [$registrationId] has status: ${currentRegistrationRequest.status}" + " can not move it to status ${request.status}"
                     )
@@ -40,10 +39,10 @@ internal class PersistRegistrationRequestHandler(
                         val newSerial = request.registrationRequest.serial
                         logger.info("Updating request [$registrationId] serial to $newSerial")
                         em.merge(createEntityBasedOnPreviousEntity(currentRegistrationRequest, newSerial))
+                    } else {
+                        // do nothing
                     }
-                    return@transaction
                 } else {
-                    // TODO remove temp logging
                     logger.info(
                         "##- Updating existing registration request '{}' from '{}' to '{}'.",
                         currentRegistrationRequest.registrationId,
@@ -51,19 +50,28 @@ internal class PersistRegistrationRequestHandler(
                         request.status
                     )
                     em.merge(createEntityBasedOnRequest(request))
-                    return@transaction
                 }
             }
+            currentRegistrationRequest
         }
+
+        if (previousEntity == null) {
+            persistNewEntity(context.holdingIdentity.toCorda(), request)
+        }
+    }
+
+    private fun persistNewEntity(holdingIdentity: HoldingIdentity, request: PersistRegistrationRequest) {
         try {
             logger.info(
                 "##- Persisting new registration request '{}' with status '{}'.",
                 request.registrationRequest.registrationId,
                 request.status
             )
-            getEntityManager(context.holdingIdentity.toCorda().shortHash).use {
+
+            getEntityManager(holdingIdentity.shortHash).use {
                 it.persist(createEntityBasedOnRequest(request))
             }
+
             logger.info(
                 "##- Persisted new registration request '{}' with status '{}'.",
                 request.registrationRequest.registrationId,
